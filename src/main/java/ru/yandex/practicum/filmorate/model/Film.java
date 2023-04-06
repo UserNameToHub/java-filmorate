84f14be25c49c3966d2c86836f182352932fc4f7
aleksandrgineyka <meinepostkam@gmail.com>
2023-04-07 01:46:07 +0300

    feat: добавить классы Film, User

---
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.Duration;
+import java.time.LocalDate;
+
+@Data
+public class Film {
+
+    public final long id;
+
+    private String name;
+
+    private String description;
+
+    private LocalDate releaseDate;
+
+    private Duration duration;
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -0,0 +1,19 @@
+package ru.yandex.practicum.filmorate.model;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class User {
+
+    private final long id;
+
+    private String email;
+
+    private String login;
+
+    private String name;
+
+    private LocalDate birthday;
+}
