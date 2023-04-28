db73a4b6944804ce881604fcb61409971bc9dc78
aleksandrgineyka <meinepostkam@gmail.com>
2023-04-29 01:50:47 +0300

    style: добавить пробелы между строчек

--- src/main/java/ru/yandex/practicum/filmorate/repository/CrudRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/CrudRepository.java
@@ -4,9 +4,14 @@ import java.util.Collection;
 import java.util.Optional;
 
 public interface CrudRepository<T> {
+
     Collection<T> findAll();
+
     Optional<T> findById(Long id);
+
     T create(T type);
+
     T update(T type);
+
     void delete(Long id);
 }
