ec36934a45cd001294c00ab63cd5fd34d38c5483
aleksandrgineyka <meinepostkam@gmail.com>
2023-04-29 01:48:11 +0300

    style: удалить пробелы между строчек

--- src/main/java/ru/yandex/practicum/filmorate/repository/CrudRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/repository/CrudRepository.java
@@ -5,12 +5,8 @@ import java.util.Optional;
 
 public interface CrudRepository<T> {
     Collection<T> findAll();
-
     Optional<T> findById(Long id);
-
     T create(T type);
-
     T update(T type);
-
     void delete(Long id);
 }
