201425430013234082f756d3b93b580fc78870e1
aleksandrgineyka <meinepostkam@gmail.com>
2023-05-29 19:25:33 +0300

    refactor: изменить реализацию метода

--- src/main/java/ru/yandex/practicum/filmorate/mapper/MpaRatingMapper.java
+++ src/main/java/ru/yandex/practicum/filmorate/mapper/MpaRatingMapper.java
@@ -8,13 +8,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 @Component
-class MpaRatingMapper implements RowMapper<Mpa> {
+public class MpaRatingMapper implements RowMapper<Mpa> {
     @Override
     public Mpa mapRow(ResultSet rs, int rowNum) throws SQLException {
-//        String mpaRating = rs.getString("rating");
-//        return  Arrays.stream(MpaRating.values())
-//                .filter(x -> x.getRating().equals(mpaRating))
-//                .findFirst().orElse(MpaRating.UNKNOWN);
-        return null;
+        return Mpa.forValues(rs.getInt("id"));
     }
 }
